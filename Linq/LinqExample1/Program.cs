1e92caeb91096053c89ef666ff1848e254efd864
eranyser <eran.weiser@gmail.com>
2026-05-05 14:21:03 +0300

    yield return

--- Linq/LinqExample1/Program.cs
+++ Linq/LinqExample1/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            List<int> result = Filter(numbers);
+            IEnumerable<int> result = Filter(numbers);
             foreach (int item in result)
             {
                 Console.WriteLine(item);
@@ -16,17 +16,15 @@
          * It iterates through each number in the input collection, checks if it is even,
          * and if so, adds it to the result list. Finally, it returns the list of even numbers.
          */
-        private static List<int> Filter(IEnumerable<int> numbers)
+        private static IEnumerable<int> Filter(IEnumerable<int> numbers)
         {
-            List<int> result = new List<int>();
             foreach (var number in numbers)
             {
                 if (number % 2 == 0)
                 {
-                    result.Add(number);
+                    yield return number;
                 }
             }
-            return result;
         }
     }
 }
